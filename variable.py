1a9402db3d45f6d31596e5393547c703f60efa3c
Панов Сергей <sergey.kyrs@mail.ru>
2024-07-01 15:22:53 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+number_of_completed_DZ = 12
+number_of_hours_spent = 1.5
+Curse_name = 'Python'
+Time_for_one_task = number_of_hours_spent / number_of_completed_DZ
+print('kyrs:', Curse_name,',всего задач:',number_of_completed_DZ,',затрачено часов:',number_of_hours_spent,',среднее время выполнения:',Time_for_one_task, 'часа.')
