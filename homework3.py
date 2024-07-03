cc7f684d36aa5c5a9539b8337d0e96491edb2211
Панов Сергей <sergey.kyrs@mail.ru>
2024-07-03 13:18:40 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,8 +1,8 @@
 name = 'Sergey'
 print('name:',name)
-age = 32
-print('age:',age)
-Age = age + 1
+Age = 32
+print('Age:',Age)
+Age = Age + 1
 print('New Age:',Age)
 is_student = True
 print('is_student:',is_student)
