b7a7031268af8c23f5c1b2b5bb0e7c2266e1f0a5
Панов Сергей <sergey.kyrs@mail.ru>
2024-07-30 13:39:26 +0300

    Add files via upload

---
+++ module_2_6.py
@@ -0,0 +1,27 @@
+# Переменная для подсчета вызовов
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()  # Увеличиваем счетчик вызовов
+    length = len(string)
+    upper_case = string.upper()
+    lower_case = string.lower()
+    return (length, upper_case, lower_case)
+
+def is_contains(string, list_to_search):
+    count_calls()  # Увеличиваем счетчик вызовов
+    # Приводим строку к нижнему регистру для сравнения
+    return string.lower() in (item.lower() for item in list_to_search)
+
+# Выводим результаты
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
+# Выводим количество вызовов
+print("Количество вызовов функций:", calls)
