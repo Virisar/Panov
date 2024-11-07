aa09d57aa89b7e9cf40fadc1c8c6b6961ca372e5
Панов Сергей <sergey.kyrs@mail.ru>
2024-11-07 11:09:16 +0300

    Add files via upload

---
+++ module_8.1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a + b
+    elif isinstance(a, str) and isinstance(b, str):
+        return a + b
+    else:
+        # Если типы разные, возвращаем строковое представление
+        return str(a) + str(b)
+
+# Примеры использования
+print(add_everything_up(123.456, 'строка'))  # 123.456строка
+print(add_everything_up('яблоко', 4215))      # яблоко4215
+print(add_everything_up(123.456, 7))           # 130.456
